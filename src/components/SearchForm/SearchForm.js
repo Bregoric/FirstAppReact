b2b84e11bb098aac330e99e666c442e055933093
Michal Frankiewicz <michal_frankiewicz@outlook.com>
2023-02-14 08:21:00 +0100

    Change Hero.js and SearchForm.js

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,12 @@
+const SearchForm = () => {
+    return ( <
+        form >
+        <
+        input type = "text" / >
+        <
+        button > Search < /button> <
+        /form>
+    );
+};
+
+export default SearchForm;
