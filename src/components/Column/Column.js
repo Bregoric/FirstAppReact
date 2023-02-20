4182cdbbbc9ece7b304b366258b7c9991c3ab8e9
Michal Frankiewicz ADM <PLFRAN_ADM@eu.miele.net>
2023-02-20 09:33:20 +0100

    Add store, initialState, add useSelector to List and Column.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,14 +1,15 @@
 import styles from './Column.module.scss'
 import Card from "../Card/Card";
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from "react-redux";
 const Column = props => {
-console.log('Column-prop' ,props);
+  const cards = useSelector(state => state.cards);
     return (
         <article className={styles.column}>
             <span className={styles.icon + ' fa fa-' + props.icon} />
             <h2 className={styles.title}>{props.title}</h2>
             <ul className={styles.cards}>
-        {props.cards.map((card) => (
+        {cards.map((card) => (
           <Card key={card.id} title={card.title} />
         ))}
       </ul>
