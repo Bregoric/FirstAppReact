00c77c3b131ce53ddc355ffa2eeea340ac9c3e6a
Michal Frankiewicz ADM <PLFRAN_ADM@eu.miele.net>
2023-02-20 09:43:54 +0100

    Add addColumn function, add id to new column, add payload.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,11 @@ import Card from "../Card/Card";
 import CardForm from '../CardForm/CardForm';
 import { useSelector } from "react-redux";
 const Column = props => {
-  const cards = useSelector(state => state.cards);
+  const cards = useSelector((state )=>
+  state.cards.filter((card) => card.columnId === props.id)
+  );
+  
+  ;
     return (
         <article className={styles.column}>
             <span className={styles.icon + ' fa fa-' + props.icon} />
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,31 +2,28 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
 
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
     const [icon, setIcon] = useState('');
+
+    const dispatch = useDispatch();
+
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title, icon: icon });
+        dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
         setTitle('');
         setIcon('');
     };
 
-    return (<form className={styles.columnForm}
-        onSubmit={handleSubmit} >
+    return ( <form className={styles.columnForm} onSubmit={handleSubmit}>
         Title:
-        <div className={styles.padding} > <TextInput value={title}
-                onChange={
-                    (e) => setTitle(e.target.value)}
-            /></div >
-        Icon: <div className={styles.padding} > <TextInput value={icon}
-            onChange={
-                (e) => setIcon(e.target.value)}
-        /></div>
-        <div className={styles.padding} > <Button> Add column </Button></div >
-        </form>
-        );
+        <div className={styles.padding}><TextInput value={title} onChange={(e) => setTitle(e.target.value)} /></div>
+        Icon: <div className={styles.padding}><TextInput value={icon} onChange={(e) => setIcon(e.target.value)} /></div>
+        <div className={styles.padding}><Button>Add column</Button></div>
+      </form>
+    );
 };
 
-        export default ColumnForm;
+export default ColumnForm;
