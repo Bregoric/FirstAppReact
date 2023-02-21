a13e8dc4c513229b0dac8500da04df14c4ab3f75
Michal Frankiewicz ADM <PLFRAN_ADM@eu.miele.net>
2023-02-21 08:18:21 +0100

    Add react router, add navbar.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List.js';
 import Container from './components/Container/Container';
+import NavBar from "./components/NavBar/NavBar";
 const App = () => {
   return (
     <Container>
+      <NavBar />
       <Hero />
       <SearchForm />
       <List />
